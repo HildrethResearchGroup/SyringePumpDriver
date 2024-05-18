00b1eb1b0c757647c769495f7c023eef2201528b
Alex Torres <altorres@mines.edu>
2024-05-17 18:07:44 -0600

    Turned Repo into xCode project

---
+++ SyringePumpDriver/SyringePumpDriverApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyringePumpDriverApp.swift
+//  SyringePumpDriver
+//
+//  Created by Steve on 5/17/24.
+//
+
+import SwiftUI
+
+@main
+struct SyringePumpDriverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
