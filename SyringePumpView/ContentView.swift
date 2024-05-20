7b22f1640b8c3cb7662322f5d6098000cb1052fa
alyssabh <94648254+alyssabh@users.noreply.github.com>
2024-05-20 11:57:12 -0600

    Create ContentView.swift
    Added content view

---
+++ SyringePumpView/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Syringe Pump 
+//
+//  Created by Steven DiGregorio on 4/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @ObservedObject var electrospinner: Electrospinner
+    
+    var body: some View {
+        HSplitView {
+            SelectableCameraView().padding()
+            VStack {
+                GeneralParameterView(electrospinner: electrospinner).padding()
+                Divider()
+                SyringePumpView(controller: electrospinner.syringePump).padding()
+                Divider()
+                VoltageSupplyView(controller: electrospinner.voltageSupply).padding()
+                Divider()
+                ElectrospinnerView(electrospinner: electrospinner).padding()
+            }.frame(minWidth: 100, idealWidth: 200, maxWidth: 300, minHeight: 400, idealHeight: 800, maxHeight: .infinity, alignment: .topLeading)
+        }
+    }
+}
